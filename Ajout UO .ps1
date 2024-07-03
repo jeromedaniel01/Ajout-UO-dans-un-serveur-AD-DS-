e476e7ef8d6bbe3b8eeed060de13795928bc2bf0
jeromeD <jeraumeonline@gmail.com>
2024-07-03 10:10:50 +0200

    Add files via upload

---
+++ Ajout UO .ps1
@@ -0,0 +1,49 @@
+#CREATION D'UNE UO 
+
+Sur le serveur ou est installé l' **AD DS** , cliquez sur le rôle **AD DS** 
+
+Clique droit sur votre serveur et choisisssez **Centre d'administration Active Directory**
+
+Sur le coté gauche ,cliquez sur le nom de votre domaine(local) 
+
+Dans la colonne de droite sous le nom de serveur(local) ,cliquez **nouveau**
+
+Puis **Unité d'organisation** 
+
+Remplir le nom  pour creer votre **UO**
+
+Votre sera créée ,vous pouvez la voir en cliquant sur le nom de votre serveur(local) en cherchant dans la colonne centrale 
+
+Cliquez sur votre **UO** et nouveau dnas l'encadré en bas a droite ,pour creer un groupe dans cette **UO**
+
+Donnez un nom a votre groupe  
+
+Une fois votre groupe créée ,double-cliquez sur le nom de votre **UO** dans l'encadré de gauche 
+
+
+Dans l'encadré en bas a droite cliquez sur nouveau (normalement  vous voyez le nom de votre **UO* en haut de l'encadré)
+
+
+Choisissez **Utilisateur** pour creer un utilisateur dans ce groupe 
+
+Renseignez les champs necessaire , attention de bien se rappeler le nom d'ouverture de session qui est utilisé pour l'ouverture de session sur un ordinateur du domaine
+
+ce nom doit ressembler a **nomchoisi@nomdedomaine**
+
+Descendez dans la fenetre d'jout Utilisateur jusqu'a l'encadré  **Membre de** et cliquez sur ajouter
+
+Dans la fenetre qui s'ouvre entrez le nom ou le debut du nom du groupe auquel l'utilisateur doit appartenir et cliquez sur **verifier les noms**
+
+choisissez le nom de groupe souhaité et votre **utilisateur** est ajouté a ce groupe 
+
+
+ 
+
+
+
+
+
+
+
+
+
